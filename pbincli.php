2d6dac709f64350bb264aa090a7763f85a8d11f6
R4SAS <r4sas@i2pd.xyz>
2020-11-09 20:06:45 +0000

    support command line options

--- pbincli.php
+++ pbincli.php
@@ -1,12 +1,10 @@
 #!/usr/bin/env php
 <?php
 /**
- * PBinPHPCLI - PrivateBin PHP CLI
+ * PBinPHPCLI - PrivateBin CLI on PHP
  *
  * That script is CLI (command line) tool for sending pastes to PrivateBin
  * instances with v2 API support.
- *
- * TODO: add work with command line arguments using getopt.
  *
  * Copyright (c) 2020 R4SAS. Code is covered by MIT license.
  */
@@ -17,31 +15,39 @@
 // PrivateBin instance URL
 $url = 'https://paste.i2pd.xyz/';
 
+// Compression mode ("zlib", "none")
+$compression = 'zlib';
+
 // Paste password
 $password = '';
 
-// Use ZLIB compression (zlib or none)
-$compression = 'zlib';
+// Paste format ("plaintext", "syntaxhighlighting", "markdown")
+$formatter = 'plaintext';
 
-// Print debug output (True or False)
-$debug = False;
+// Paste expire ("5min", "10min", "1hour", "1day", "1week", "1month", "1year", "never")
+$expire = '10min';
+
+// Enable discussion (true, false)
+$discussion = false;
+
+// Burn paste after reading (true, false)
+$burn = false;
+
+// Debug output (true, false)
+$debug = false;
 
 /* --------------------- Don't touch anything below! ----------------------- */
 /* ------------------------------------------------------------------------- */
 
-// Paste data -- testing
-$paste = array(
-  'paste' => 'text content of the paste'
-);
 
 /* --- Check if requirements are met --- */
 
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
-  exit("PHP 7+ is required!");
+  exit("PHP 7+ is required!" . PHP_EOL);
 }
 
 if (function_exists('\bcmul') === false) {
-  exit("That tool requires BCMath extension!");
+  exit("That tool requires BCMath extension!" . PHP_EOL);
 }
 
 /**
@@ -54,10 +60,6 @@ function b58(string $string)
 {
   $alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
   $base = strlen($alphabet);
-
-  if (is_string($string) === false) {
-    exit('Argument $string must be a string.');
-  }
 
   if (strlen($string) === 0) {
     return '';
@@ -95,6 +97,35 @@ function b58(string $string)
   return (string) $output;
 }
 
+/* --- Help --- */
+function help() {
+  echo <<<EOL
+PBinPHPCLI - PrivateBin CLI on PHP
+
+Available options:
+1. Used with required input
+  -t "text"      - text in quotes, ignored if used stdin
+  -f "file.ext"  - include attachment to paste using pointed filename
+  -p "password"  - password for encrypting paste
+  -E "1day"      - paste lifetime
+  -F "plaintext" - paste formatter on webpage in reading mode
+  -c "zlib"      - compression mode
+  -s "https://paste.i2pd.xyz/"
+                 - PrivateBin instance address
+
+2. Used as switches
+  -b             - Bypass hardcoded values in "-E" and "-F" options
+  -B             - Burn after reading
+  -D             - Enable discussion in paste
+  -d             - Enable debug output
+
+3. Use text input from stdin
+  -T             - input text when no "-t" flag used or text input done
+                   with pipe. Can be skipped if "-t" used or when file with
+                   "-f" specified.
+EOL;
+  exit(PHP_EOL);
+}
 
 /* --- Encryption parameters --- */
 $CIPHER_ITER_COUNT  = 100000;
@@ -103,13 +134,91 @@ $CIPHER_BLOCK_BITS  = 256;
 $CIPHER_BLOCK_BYTES = $CIPHER_BLOCK_BITS / 8; // 32 bytes
 $CIPHER_TAG_BITS    = $CIPHER_BLOCK_BITS / 2; // 128 bits
 $CIPHER_TAG_BYTES   = $CIPHER_TAG_BITS / 8;   // 16 bytes
-$CIPHER_STRONG      = True;
-
+$CIPHER_STRONG      = true;
 
 /* --- Main code --- */
-if (substr($url, -1) !== '/') {
-  // URL must ends with slash!
-  $url .= '/';
+// Get arguments from command line
+$shortopts = 'bt:Tf:p:E:BDF:c:s:dh';
+$opts = getopt($shortopts);
+
+if(empty($opts) || isset($opts['h'])) {
+  help();
+}
+
+// Bypass mode check
+if(isset($opts['b'])) $bypass = true;
+
+$textinputed = false;
+$paste = array();
+
+foreach($opts as $opt => $value) {
+  switch($opt) {
+    case 't':
+      if(isset($opts['T']) || isset($paste['paste'])) continue;
+      $paste = array_merge($paste, ["paste" => $value]);
+      continue;
+    case 'T':
+      $input_data = stream_get_contents(STDIN);
+      $paste = array_merge($paste, ["paste" => $input_data]);
+      continue;
+    case 'f':
+      $file = file_get_contents($value);
+      if ($file) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_buffer($finfo, $file);
+        if (!$mime) {
+          $mime = "application/octet-stream";
+        }
+        $data = "data:" . $mime . ";base64," . base64_encode($file);
+        $name = basename($value);
+        $paste = array_merge($paste, ["attachment" => $data, "attachment_name" => $name]);
+      } else {
+        exit("Unable open file " . $value . PHP_EOL);
+      }
+      continue;
+    case 'p':
+      $password = $value;
+      continue;
+    case 'E':
+      if (in_array($value, ["5min", "10min", "1hour", "1day", "1week", "1month", "1year", "never"]) || $bypass) {
+        $expire = $value;
+      }
+      continue;
+    case 'B':
+      if($discussion) exit("You can't mess discussion and burn flags!" . PHP_EOL);
+      $burn = true;
+      continue;
+    case 'D':
+      if($burn) exit("You can't mess discussion and burn flags!" . PHP_EOL);
+      $discussion = true;
+      continue;
+    case 'F':
+      if(in_array($value, ["plaintext", "syntaxhighlighting", "markdown"]) || $bypass) {
+        $formatter = $value;
+      }
+      continue;
+    case 'c':
+      if(in_array($value, ["zlib", "none"])) {
+        $compression = $value;
+      }
+      continue;
+    case 's':
+      $url = $value;
+      if (substr($url, -1) !== '/') {
+        // URL must ends with slash!
+        $url .= '/';
+      }
+      continue;
+    case 'd':
+      $debug = true;
+      continue;
+    default:
+      continue;
+  }
+}
+
+if(!isset($paste['paste']) && !isset($paste['attachment'])) {
+  exit("Nothing to send!" . PHP_EOL);
 }
 
 $passbytes = openssl_random_pseudo_bytes($CIPHER_BLOCK_BYTES, $CIPHER_STRONG);
@@ -136,9 +245,9 @@ $adata = array(
     'gcm',
     $compression,
   ),
-  'plaintext',
-  0,
-  0
+  $formatter,
+  (int) $discussion,
+  (int) $burn
 );
 $authdata = json_encode($adata, JSON_UNESCAPED_SLASHES);
 
@@ -154,7 +263,7 @@ $fulldata = array(
   'adata' => $adata,
   'ct' => base64_encode($cipherText . $tag),
   'meta' => array(
-    'expire' => '5min'
+    'expire' => $expire
   ),
   'v' => 2
 );
@@ -164,7 +273,8 @@ $data = json_encode($fulldata, JSON_UNESCAPED_SLASHES);
 if($debug) {
   echo "Passhash:\t"   . $passhash . PHP_EOL;
   echo "PBKDF2 Key:\t" . base64_encode($key) . PHP_EOL;
-  echo "Paste Data:\t" . print_r($pastedata, true) . PHP_EOL;
+  echo "Paste Data:\t" . print_r($paste, true);
+  //echo "Paste Data:\t" . print_r($pastedata, true) . PHP_EOL;
   echo "Auth Data:\t"  . print_r($authdata, true) . PHP_EOL;
   echo "CipherText:\t" . base64_encode($cipherText) . PHP_EOL;
   echo "CipherTag:\t"  . base64_encode($tag) . PHP_EOL;
